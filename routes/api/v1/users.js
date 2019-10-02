934971133f573c352b09f77c37aa544ede0b8e7d
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-10-01 21:26:55 -0600

    Require secureRandom to generate api key upon creating account

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -3,13 +3,15 @@ var router = express.Router();
 
 var user = require('../../../models').User
 
+const secureRandom = require('secure-random')
+
 /* POST user account creation */
 router.post('/', function (req, res, next) {
   res.setHeader("Content-type", "application/json")
   user.create({
     email: req.params.email,
-    password: req.params.password,
-    password_confirmation: req.params.password,
+    password_digest: req.params.password,
+    api_key: secureRandom(10, {type: 'Uint8Array'}),
   })
   .then(user => {
     res.status(201).send(user.api_key)
