81bb10b9659818873293a650ef894253088db882
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-10-05 14:20:56 -0600

    Postman causing errors for response being sent

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var body = require('morgan-body');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/api/v1/users');
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,33 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
-var user = require('../../../models').User
-const uuid = require('uuidv4').default;
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+var User = require('../../../models').User;
+var uuid = require('uuidv4').default;
+var bcrypt = require('bcrypt');
+var saltRounds = 10;
 
 /* POST user account creation */
-router.post('/', function (req, res, next) {
-  res.setHeader("Content-type", "application/json")
-  if ((req.params.email && req.params.password && req.params.password_confirmation) && (req.params.password == req.params.password_confirmation))
-  return
-  bcrypt.hash(req.params.password, saltRounds, function(err, hash) {
-    user.create({
-      email: req.params.email,
-      passwordDigest: req.params.password,
-      apiKey: uuid()
-    })
-    .then(user => {
-      res.status(201).send(JSON.stringify({
-        status: res.statusCode,
-        apiKey: user.apiKey
-      }))
-    })
-    .catch(error => {
-      console.log(error)
-      res.status(500).send({error})
-    })
-  })
+router.post("/", function(req, res, next) {
+  if (req.body.email && req.body.password && req.body.password_confirmation) {
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      User.create({
+        email: req.body.email,
+        passwordDigest: hash,
+        apiKey: uuid()
+      })
+      .then(user => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(201).send(JSON.stringify({apiKey: user.apiKey}));
+      })
+      .catch(error => {
+        res.status(500).send({error})
+      });
+    });
+  } else {
+    res.status(500).send("Account cannot be created")
+  }
 });
 
 module.exports = router;
