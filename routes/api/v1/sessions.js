1518f121c9f40fb654fb21a36409918c66789b17
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-10-05 14:58:44 -0600

    Postman causing issues again but tests passing

--- routes/api/v1/sessions.js
+++ routes/api/v1/sessions.js
@@ -3,28 +3,30 @@ var router = express.Router();
 var user = require('../../../models').User
 const bcrypt = require('bcrypt');
 
-router.post("/", function (req, res, next) {
-  res.setHeader("Content-type", "application/json")
-  if (req.body.email && req.body.password)
-  user.findOne({
-    where: {email: req.body.email}
-  })
-  .then(user => {
-    trueUser = (bcrypt.compareSync(req.body.password, user.passwordDigest))
-    if (trueUser) {
-      res.status(200).send(JSON.stringify({
-        status: res.statusCode,
-        apiKey: user.apiKey
-      }))
-    } else {
-      res.status(401).send(JSON.stringify("Access Denied"))
-    }
-  })
-  .catch(error => {
-    console.log('the error', error)
-    res.status(500).send({error})
-  })
-})
+router.post("/", function(req, res, next) {
+  res.setHeader("Content-Type", "application/json")
+  if (req.body.email && req.body.password) {
+    user.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+    .then(user => {
+      trueUser = bcrypt.compareSync(req.body.password, user.passwordDigest)
+      console.log(req.body.password, user.passwordDigest)
+        if (trueUser) {
+          res.status(200).send({
+            apiKey: user.apiKey
+          })
+        } else {
+          res.status(401).send("Access denied")
+        }
+    })
+    .catch(error => {
+      res.status(500).send({error})
+    })
+  }
+});
 
 
 module.exports = router;
