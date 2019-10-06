4b9326b7dfd42f9838f360db7e99229acf47cf82
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-10-06 17:14:40 -0600

    Add fetch to forecast file to define it

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 var User = require('../../../models').User;
 var uuid = require('uuidv4').default;
-var bcrypt = require('bcrypt');
-var saltRounds = 10;
+// var bcrypt = require('bcrypt');
+// var saltRounds = 10;
+const fetch = require("node-fetch");
 
 router.get("/", function(req, res, next) {
   if (req.body.apiKey) {
@@ -12,10 +13,23 @@ router.get("/", function(req, res, next) {
       where: {apiKey: req.body.apiKey}
     });
     fetch("https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.location}&key=${process.env.GOOGLE-API-KEY}")
+    .then(response => response.json())
     .then(coords => {
-      lat: response.results.geometry.location.lat,
-      long: response.results.geometry.location.lng 
-    }
+      let lat = response.results[0].geometry.location.lat
+      let long = response.results[0].geometry.location.lng
+
+      fetch("https://api.darksky.net/forecast/${process.env.DARKSKY-API-KEY}/${lat},${long}")
+      .then(dsResponse => dsResponse.json())
+      .then(response => {
+        res.status(200).send(JSON.stringify({
+          // console.log(res)
+          location: res.location,
+          current: res.currently,
+          hourly: res.hourly,
+          daily: res.daily
+        }))
+      })
+    });
   } else {
     res.status(401).send("Unauthorized")
   }
