3040bb7a98722a22a837f5054f47ed9e6c40cfbe
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-10-06 22:29:33 -0600

    Delete commented out text

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -6,33 +6,11 @@ const fetch = require("node-fetch");
 var http = require("http");
 require('dotenv').config();
 
-
-var geocodeOptions = {
-  host: 'https://maps.googleapis.com/maps/api/geocode/json',
-  path: "?address=${req.query.location}&key=${process.env.GOOGLE-API-KEY}",
-  method: 'GET'
-};
-
 var darkskyOptions = {
   host: "https://api.darksky.net/forecast",
   path: "/${process.env.DARKSKY-API-KEY}/${lat},${long}",
   method: 'GET'
 }
-
-// function getJSON(options, cb) {
-//   http.request(geocodeOptions, function(res) {
-//     var body = '';
-//
-//     res.on('data', function(chunk) {
-//       body+= chunk;
-//     });
-//
-//     res.on('end', function() {
-//       var coords = JSON.parse(body);
-//       console.log(coords);
-//     })
-//   }).end()
-// }
 
 router.get("/", function(req, res, next) {
   res.setHeader("Content-Type", "application/json")
